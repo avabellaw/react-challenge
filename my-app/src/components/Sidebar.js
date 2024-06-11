cdec7334942b10b53e7bead995e062cb0ba09a08
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-11 16:15:37 +0100

    Create functional component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+    return (
+        <div>
+            <a>MyPhotos</a>
+            <a>My Illustrations</a>
+            <a>My Paintings</a>
+        </div>
+    );
+};
+
+export default Sidebar;
